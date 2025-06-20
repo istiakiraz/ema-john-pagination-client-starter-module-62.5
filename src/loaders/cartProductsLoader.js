60ac2819524843e8881aaf0ffd71d054db231869
istiakiraz <istiakiraz@gmail.com>
2025-06-21 01:26:26 +0600

    added a some advance topic

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,16 +1,28 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
+
+     // if cart data is in database, you have to use async await
+    const storedCart = getShoppingCart();
+    const storeCartIds = Object.keys(storedCart)
+    console.log(storeCartIds);
+
+    const loadedProducts = await fetch('http://localhost:5000/productsByIds',{
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(storeCartIds)
+    });
     const products = await loadedProducts.json();
 
-    // if cart data is in database, you have to use async await
-    const storedCart = getShoppingCart();
-
+   
     const savedCart = [];
 
+    
+
     for (const id in storedCart) {
-        const addedProduct = products.find(pd => pd._id === id);
+        const addedProduct = products?.find(pd => pd._id === id);
         if (addedProduct) {
             const quantity = storedCart[id];
             addedProduct.quantity = quantity;
